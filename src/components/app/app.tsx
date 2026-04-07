705ba2f9b27aa653f3570ae389c612f8240e6041
Aliaksei Podobny <podobnyaliaksei@yandex.by>
2026-04-07 15:04:02 +0300

    v4_fix:profile_order_modal_and_login_preloader

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -32,9 +32,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    if (getCookie('accessToken')) {
-      dispatch(getUserData());
-    }
+    dispatch(getUserData());
     dispatch(getIngredients());
     dispatch(getFeed());
   }, [dispatch]);
